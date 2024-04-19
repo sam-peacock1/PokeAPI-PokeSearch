8399d2d53af28915652e36e0091a6ece6da5cbd6
Jacob Greenberg <101679277+Jacob-Greenberg@users.noreply.github.com>
2024-04-19 11:12:06 -0400

    Adding debug line

--- main.py
+++ main.py
@@ -52,5 +52,6 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 if __name__ == '__main__':
+    print(f"PokeSearch running on {host}:{port}")
     app.run(host=host, port=port, debug=True)
 
